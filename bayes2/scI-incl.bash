9f86967a5764fc79d212cdfd41eff26ebb05a030
Danny van Dyk <danny.van.dyk@gmail.com>
2013-07-05 20:01:59 +0200

    Add scI-incl

---
+++ bayes2/scI-incl.bash
@@ -0,0 +1,11 @@
+#vim: set sts=4 et :
+
+source job.bash
+
+export MCMC_PRERUN_SAMPLES=4000
+export MCMC_PRERUN_CHAINS=12
+export MCMC_PRERUN_PARALLEL=1
+
+export PMC_MONO_FINAL_CHUNKSIZE=120000
+
+main $@
