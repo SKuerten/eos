2ffb36b6f4aecc5d6157b075b2aa22a71f34f2da
Danny van Dyk <danny.van.dyk@gmail.com>
2013-07-06 14:15:24 +0200

    Add scI-excl

---
+++ bayes2/scI-excl.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+#vim: set sts=4 et :
+
+echo @=$@
+
+env
+
+
+source ${EOS_SCRIPT_PATH}/job.bash
+
+export MCMC_PRERUN_SAMPLES=40000
+export MCMC_PRERUN_UPDATE_SIZE=1000
+export MCMC_PRERUN_CHAINS=12
+export MCMC_PRERUN_PARALLEL=1
+
+export PMC_MONO_FINAL_CHUNKSIZE=120000
+
+main $@
