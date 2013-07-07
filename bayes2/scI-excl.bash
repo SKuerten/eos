bdd65857e8172e4c49c03eaf9eb31dae07b660e1
Danny van Dyk <danny.van.dyk@gmail.com>
2013-07-07 11:01:08 +0200

    Make sci-excl ignore supressed modes

--- bayes2/scI-excl.bash
+++ bayes2/scI-excl.bash
@@ -1,11 +1,6 @@
 #!/bin/bash
 #vim: set sts=4 et :
 
-echo @=$@
-
-env
-
-
 source ${EOS_SCRIPT_PATH}/job.bash
 
 export MCMC_PRERUN_SAMPLES=40000
@@ -14,5 +9,9 @@ export MCMC_PRERUN_CHAINS=12
 export MCMC_PRERUN_PARALLEL=1
 
 export PMC_MONO_FINAL_CHUNKSIZE=120000
+export PMC_MONO_IGNORE_GROUPS="
+    --pmc-ignore-group 0
+    --pmc-ignore-group 3
+"
 
 main $@
