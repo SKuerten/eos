f645d286291741293d744b8442654f868271e91c
Danny van Dyk <danny.van.dyk@gmail.com>
2013-08-20 15:34:29 +0200

    Fix log-gamma -> gaussian for B->B::mu_pi^2@1GeV

--- bayes2/priors.bash
+++ bayes2/priors.bash
@@ -71,7 +71,7 @@ export NUISANCE_B_TO_PLL_SUBLEADING="
 "
 
 export NUISANCE_B_TO_XS_HQE="
-    --nuisance    B->B::mu_pi^2@1GeV                             0.0   2.0  ${N_SIGMAS}     --prior    log-gamma   0.35      0.45      0.55
+    --nuisance    B->B::mu_pi^2@1GeV                             0.0   2.0  ${N_SIGMAS}     --prior    gaussian    0.35      0.45      0.55
     --nuisance    B->B::mu_G^2@1GeV                              0.0   2.0  ${N_SIGMAS}     --prior    log-gamma   0.33      0.35      0.38
 "
 
