3482b39209eabe65283eab87889d50b0eabef2ed
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2013-08-23 15:47:02 +0200

    [merge] Add --cut-off option to ignore chains far out in the tails

--- plot/merge.py
+++ plot/merge.py
@@ -14,7 +14,21 @@ def natural_sort_key(key):
     import re
     return [int(t) if t.isdigit() else t for t in re.split(r'(\d+)', key)]
 
-def merge_preruns(output_file_name, search='*.hdf5', input_files=None, strict=False, compression=False, use_pytables=False):
+def invalid_chain(file, chain, cut_off=None):
+    """Check if mode larger than cut off"""
+    if cut_off is None:
+        return False
+    # last value of the last mode is the best found in all updates sequences
+    mode = file['/prerun/chain #%d/stats/mode' % chain][-1][-1]
+
+    if mode > cut_off:
+        return False
+    else:
+        return True
+
+def merge_preruns(output_file_name, search='*.hdf5', input_files=None,
+                  strict=False, compression=False, use_pytables=False,
+                  cut_off=None):
     """
     Merge a prerun, stored in different files into one common file.
     Before:
@@ -31,7 +45,7 @@ def merge_preruns(output_file_name, search='*.hdf5', input_files=None, strict=Fa
         search_results = natsorted(glob.glob(os.path.join(base_dir, search)))
     else:
         search_results = input_files
-        
+
     if len(search_results) == 0:
         raise Exception("No files matching '%s' in %s were found!" % (search, base_dir))
 
@@ -42,10 +56,15 @@ def merge_preruns(output_file_name, search='*.hdf5', input_files=None, strict=Fa
     cmd += ' -o %s %s' % (search_results[0], output_file_name)
     commands.getstatusoutput(cmd)
     """
+
+    # find first valid chain
+    if cut_off is not None:
+        print("Warning: first file %s is merged w/o checking. " % search_results[0] +
+              "If it contains an invalid chain use --input-file-list to reorder manually!")
     shutil.copy(search_results[0], output_file_name)
 
     groups = ['/prerun', '/descriptions/prerun']
-    
+
     if use_pytables:
         import tables as pytables
         output_file = pytables.openFile(output_file_name, "r+")
@@ -55,7 +74,7 @@ def merge_preruns(output_file_name, search='*.hdf5', input_files=None, strict=Fa
         output_file = h5py.File(output_file_name, "r+")
         chains_per_file = len(output_file[groups[0]].keys())
     for f_i, file_name in enumerate(search_results[1:]):
-        
+
         print("merging %s into %s" % (file_name, output_file_name))
         # check agreement
         if strict:
@@ -65,7 +84,7 @@ def merge_preruns(output_file_name, search='*.hdf5', input_files=None, strict=Fa
             assert(len(input_file.getNode(groups[1] + '/chain #0/constraints')) == len(output_file.getNode(groups[1] + '/chain #0/constraints')))
             assert(len(input_file.getNode(groups[1] + '/chain #0/parameters')) == len(output_file.getNode(groups[1] + '/chain #0/parameters')))
             input_file.close()
-        
+
         # copy data
         for g in groups:
             for i in range(chains_per_file):
@@ -79,40 +98,44 @@ def merge_preruns(output_file_name, search='*.hdf5', input_files=None, strict=Fa
                         raise Exception("ptrepack reported:\n %s" % output)
                 else:
                     input_file = h5py.File(file_name, 'r')
+                    if g == '/prerun' and invalid_chain(input_file, i, cut_off):
+                        print("skipping chain %d in %s" % (i, file_name))
+                        continue
+
                     input_file.copy(g + "/chain #%d" % i, output_file, name=g + "/chain #%d" % ((f_i + 1) * chains_per_file + i))
                     input_file.close()
-                    
-    print("Merged %d files together" % len(search_results))
+
+    print("Merged chains of %d files together" % len(search_results))
 
     output_file.close()
 
 def merge_sm_unc(output_file_name, input_files):
     """
-    
+
     """
     import tables as pytables
-    
+
     print("merging %s into %s" % (input_files[0], output_file_name))
     shutil.copy(input_files[0], output_file_name)
     optional_groups = ['/data/parameters']
     groups = ['/data/observables']
-    
+
     output_file = pytables.openFile(output_file_name, "r+")
     n_obs = len(output_file.getNode('/descriptions/observables')._f_listNodes())
     n_par = len(output_file.getNode('/descriptions/parameters')._f_listNodes())
     n_samples = len(output_file.getNode('/data/observables'))
-    
-    
+
+
     for f_i, file_name in enumerate(input_files[1:]):
         print("merging %s into %s" % (file_name, output_file_name))
 
         input_file = pytables.openFile(file_name, 'r')
-        
+
         # check agreement
         assert(n_obs == len(input_file.getNode('/descriptions/observables')._f_listNodes()))
         assert(n_par == len(input_file.getNode('/descriptions/parameters')._f_listNodes()))
         assert(n_samples == len(input_file.getNode('/data/observables')))
-        
+
         # copy/append data
         for g in groups:
             output_file.getNode(g).append(input_file.getNode(g).read())
@@ -122,13 +145,13 @@ def merge_sm_unc(output_file_name, input_files):
                output_file.getNode(g).append(input_file.getNode(g).read())
            except pytables.exceptions.NoSuchNodeError:
                pass
-           
+
         input_file.close()
     output_file.close()
     return
-                
+
     # h5py doesn't work with nested arrays, see http://code.google.com/p/h5py/issues/detail?id=211
-    """            
+    """
     out_ds = output_file[g]
     out_ds.resize((n + input_file[g].len(),))
     for i, rec in enumerate(input_file[g]):
@@ -138,33 +161,34 @@ def merge_sm_unc(output_file_name, input_files):
     """
 
 def main():
-    
+
     import argparse
 
     parser  = argparse.ArgumentParser(description='Merge MCMC input files')
-    parser.add_argument('--descriptions', dest='desc',
-                        help='Copy descriptions from this HDF5 input file to output', action='store')
-    parser.add_argument('--search', dest='search',
-                        help='HDF5 input file name pattern', action='store', default='*.hdf5')
-    parser.add_argument('--output',
-                        help='Output file name', action='store')
-    parser.add_argument('--strict', dest='strict', default=False,
-                        help='Check congruence', action='store_true')
     parser.add_argument('--compression', dest='compression', default=0,
                         help='Compress the copied data with a compression level. Default: 0', action='store')
+    parser.add_argument('--cut-off', help='Skip chains whose maximum posterior is below the cut-off', action='store', default=None)
+    parser.add_argument('--descriptions', dest='desc',
+                        help='Copy descriptions from this HDF5 input file to output', action='store')
     parser.add_argument('--input-file-list', dest='input_file_list',
                         help='List input files as text, one line per file in a file', action='store')
+    parser.add_argument('--output',
+                        help='Output file name', action='store')
+    parser.add_argument('--search', dest='search',
+                        help='HDF5 input file name pattern', action='store', default='*.hdf5')
     parser.add_argument('--sm-unc', dest='sm_unc',
                         help='Merge uncertainty propagation files', action='store_true')
-    
+    parser.add_argument('--strict', dest='strict', default=False,
+                        help='Check congruence', action='store_true')
+
     args = parser.parse_args()
 
     if args.output is None:
         args.output = os.path.join(os.getcwd(), 'mcmc_pre_merged.hdf5')
-    
+
     if args.desc is not None:
         import h5py
-        
+
         input_file = h5py.File(args.desc, 'r')
         output_file = h5py.File(args.output, 'r+')
         try:
@@ -174,21 +198,23 @@ def main():
             pass
         input_file.copy('/descriptions', output_file['/'])
         return
-    
+
     print("Merging into output file %s" % args.output)
-    
+
     input_files = None
     if args.input_file_list is not None:
         f = open(args.input_file_list, 'r')
         #remove trailing newline
         input_files = [name[:-1] for name in f.readlines()]
-        
+
     if args.sm_unc:
         merge_sm_unc(output_file_name=args.output, input_files=input_files)
     else:
         # default: merge mcmc
-        merge_preruns(output_file_name=args.output, search=args.search, input_files=input_files, strict=args.strict, compression=int(args.compression))
-    
+        merge_preruns(output_file_name=args.output, search=args.search,
+                      input_files=input_files, strict=args.strict,
+                      compression=int(args.compression), cut_off=args.cut_off)
+
 if __name__ == '__main__':
 #    merge_preruns(search='scenario2*.hdf5')
     main()
