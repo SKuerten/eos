f5ebf4d6a74e3d6ac01d8253bd4ad3b25cc40ff8
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2013-08-23 16:42:55 +0200

    [merge] Drop support for pytables. Fix merging when skipping chains

--- plot/merge.py
+++ plot/merge.py
@@ -2,6 +2,7 @@
 
 import commands
 import glob
+import h5py
 import os
 import shutil
 
@@ -28,8 +29,7 @@ def invalid_chain(file, chain, cut_off=None):
         return True
 
 def merge_preruns(output_file_name, search='*.hdf5', input_files=None,
-                  strict=False, compression=False, use_pytables=False,
-                  cut_off=None):
+                  compression=False, use_pytables=False, cut_off=None):
     """
     Merge a prerun, stored in different files into one common file.
     Before:
@@ -50,14 +50,6 @@ def merge_preruns(output_file_name, search='*.hdf5', input_files=None,
     if len(search_results) == 0:
         raise Exception("No files matching '%s' in %s were found!" % (search, base_dir))
 
-    #create merge file
-    """
-    cmd = '/afs/ipp/home/f/fdb/.local/bin/ptrepack'
-    cmd += ' --complevel=%d' % (compression if compression is not None else 0,)
-    cmd += ' -o %s %s' % (search_results[0], output_file_name)
-    commands.getstatusoutput(cmd)
-    """
-
     # find first valid chain
     if cut_off is not None:
         print("Warning: first file %s is merged w/o checking. " % search_results[0] +
@@ -66,46 +58,27 @@ def merge_preruns(output_file_name, search='*.hdf5', input_files=None,
 
     groups = ['/prerun', '/descriptions/prerun']
 
-    if use_pytables:
-        import tables as pytables
-        output_file = pytables.openFile(output_file_name, "r+")
-        chains_per_file = len(output_file.listNodes(groups[0]))
-    else:
-        import h5py
-        output_file = h5py.File(output_file_name, "r+")
-        chains_per_file = len(output_file[groups[0]].keys())
-    for f_i, file_name in enumerate(search_results[1:]):
+    # count chains copied
+    nchains_copied = 0
 
+    output_file = h5py.File(output_file_name, "r+")
+    nchains_copied = len(output_file[groups[0]].keys())
+
+    for f_i, file_name in enumerate(search_results[1:]):
         print("merging %s into %s" % (file_name, output_file_name))
-        # check agreement
-        if strict:
-            input_file = pytables.openFile(file_name,'r')
-            print(chains_per_file)
-            assert(chains_per_file == len(input_file.listNodes(groups[1])))
-            assert(len(input_file.getNode(groups[1] + '/chain #0/constraints')) == len(output_file.getNode(groups[1] + '/chain #0/constraints')))
-            assert(len(input_file.getNode(groups[1] + '/chain #0/parameters')) == len(output_file.getNode(groups[1] + '/chain #0/parameters')))
-            input_file.close()
+        input_file = h5py.File(file_name, 'r')
+        nchains_in_file = len(input_file[groups[0]].keys())
 
         # copy data
-        for g in groups:
-            for i in range(chains_per_file):
-                if use_pytables:
-                    cmd = 'ptrepack'
-                    cmd += ' --complevel=%d' % int(compression or 0)
-                    cmd += ' %s:%s/chain\ #%d %s:%s/chain\ #%d' % (file_name, g, i, output_file_name, g, (f_i + 1) * chains_per_file + i)
-                    print(cmd)
-                    status, output = commands.getstatusoutput(cmd)
-                    if status != 0:
-                        raise Exception("ptrepack reported:\n %s" % output)
-                else:
-                    input_file = h5py.File(file_name, 'r')
-                    if g == '/prerun' and invalid_chain(input_file, i, cut_off):
-                        continue
+        for i in range(nchains_in_file):
+            if invalid_chain(input_file, i, cut_off):
+                continue
+            for g in groups:
+                input_file.copy(g + "/chain #%d" % i, output_file, name=g + "/chain #%d" % nchains_copied)
+            nchains_copied += 1
+        input_file.close()
 
-                    input_file.copy(g + "/chain #%d" % i, output_file, name=g + "/chain #%d" % ((f_i + 1) * chains_per_file + i))
-                    input_file.close()
-
-    print("Merged chains of %d files together" % len(search_results))
+    print("Merged %d chains of %d files" % (nchains_copied, len(search_results)))
 
     output_file.close()
 
@@ -178,8 +151,6 @@ def main():
                         help='HDF5 input file name pattern', action='store', default='*.hdf5')
     parser.add_argument('--sm-unc', dest='sm_unc',
                         help='Merge uncertainty propagation files', action='store_true')
-    parser.add_argument('--strict', dest='strict', default=False,
-                        help='Check congruence', action='store_true')
 
     args = parser.parse_args()
 
@@ -212,7 +183,7 @@ def main():
     else:
         # default: merge mcmc
         merge_preruns(output_file_name=args.output, search=args.search,
-                      input_files=input_files, strict=args.strict,
+                      input_files=input_files,
                       compression=int(args.compression), cut_off=float(args.cut_off))
 
 if __name__ == '__main__':
