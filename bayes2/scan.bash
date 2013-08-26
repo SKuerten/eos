2700fed9fc2dd4262b57276068b55fe8840c6731
Danny van Dyk <danny.van.dyk@gmail.com>
2013-08-26 20:25:03 +0200

    Replace existing scenarios II-IV with new ones

--- bayes2/scan.bash
+++ bayes2/scan.bash
@@ -10,25 +10,20 @@ export SCAN_scI="
 
 export SCAN_scII="
         --scan      Re{c7}      -2.0     +2.0              --prior flat
-        --scan      Re{c7'}     -2.0     +2.0              --prior flat
+        --scan      Im{c7}      -2.0     +2.0              --prior flat
+        --scan      Re{c9}     -15.0    +15.0              --prior flat
+        --scan      Im{c9}     -15.0    +15.0              --prior flat
+        --scan      Re{c10}    -15.0    +15.0              --prior flat
+        --scan      Im{c10}    -15.0    +15.0              --prior flat
 "
 
 export SCAN_scIII="
-        --scan      Re{c7}      -2.0     +2.0              --prior flat
-        --scan      Re{c7'}     -2.0     +2.0              --prior flat
-        --scan      Re{c9}     -15.0    +15.0              --prior flat
-        --scan      Re{c9'}    -15.0    +15.0              --prior flat
-        --scan      Re{c10}    -15.0    +15.0              --prior flat
-        --scan      Re{c10'}   -15.0    +15.0              --prior flat
-"
-
-export SCAN_scIV="
-        --scan      Re{c10}    -15.0    +15.0              --prior flat
-        --scan      Re{c10'}   -15.0    +15.0              --prior flat
-"
-
-export SCAN_scV="
-        --scan      Re{c10}    -15.0    +15.0              --prior flat
+        --scan      Re{c7}      -1.0     +1.0              --prior flat
+        --scan      Re{c9}      -7.5     +7.5              --prior flat
+        --scan      Re{c10}     -7.5     +7.5              --prior flat
+        --scan      Re{c7'}     -1.0     +1.0              --prior flat
+        --scan      Re{c9'}     -7.5     +7.5              --prior flat
+        --scan      Re{c10'}    -7.5     +7.5              --prior flat
 "
 
 export SCAN_sm=""
