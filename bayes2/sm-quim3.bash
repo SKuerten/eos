c989f67d5b59c8f58484b98d0375e72ad822079f
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2013-08-26 10:37:54 +0200

    [sm-quim] PMC main run at C2PAP

--- bayes2/sm-quim3.bash
+++ bayes2/sm-quim3.bash
@@ -8,6 +8,12 @@ export MCMC_PRERUN_UPDATE_SIZE=500
 export MCMC_PRERUN_CHAINS=1
 export MCMC_PRERUN_PARALLEL=0
 
+# more components as there are two modes, but chains can see both
+export PMC_CHUNKSIZE=1500
+export PMC_CLUSTERS=50
+export PMC_CONVERGENCE="$PMC_CONVERGENCE --pmc-crop-highest-weights 20"
 export PMC_FINAL_CHUNKSIZE=1000000
+export PMC_GROUP_BY_RVALUE=1.5
+export PMC_INITIALIZATION="$PMC_INITIALIZATION --pmc-r-value-no-nuisance 0"
 
 main $@
