02fe654619fdfb6c1809b9f5ce86bc99499b126c
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2013-09-03 17:40:37 +0200

    [scV-bsmumu, sm-uncVLL] Update to test queue support with loadleveler

--- bayes2/scV-bsmumu.bash
+++ bayes2/scV-bsmumu.bash
@@ -4,7 +4,7 @@
 source ${EOS_SCRIPT_PATH}/job.bash
 
 export MCMC_PRERUN_SAMPLES=10000
-export MCMC_PRERUN_CHAINS=10
+export MCMC_PRERUN_CHAINS=5
 export MCMC_PRERUN_PARALLEL=1
 
 export PMC_CLUSTERS=5
@@ -12,6 +12,10 @@ export PMC_FINAL_CHUNKSIZE=50000
 
 export PMC_NUMBER_OF_JOBS=3
 export SGE_FINAL_QUEUE=short
-export PMC_POLLING_INTERVAL=5
+export PMC_POLLING_INTERVAL=2
+
+export PMC_RESOURCE_MANAGER=loadleveler
+export LL_QUEUE=shorttest
+export LL_FINAL_QUEUE=${LL_QUEUE}
 
 main $@
--- bayes2/sm-uncVLL.bash
+++ bayes2/sm-uncVLL.bash
@@ -10,8 +10,8 @@ export MCMC_PRERUN_PARALLEL=1
 export PMC_SEED=13614623
 
 export PMC_CHUNKSIZE=1000
-export PMC_CLUSTERS=25
-export PMC_FINAL_CHUNKSIZE=1000000
+export PMC_CLUSTERS=35
+export PMC_FINAL_CHUNKSIZE=100000
 export PMC_GROUP_BY_RVALUE=1.5
 export PMC_INITIALIZATION="$PMC_INITIALIZATION --pmc-r-value-no-nuisance 0"
 
@@ -21,5 +21,13 @@ export UNC_PMC_INPUT="
     --pmc-input ${BASE_NAME}/sm_uncVLL/pmc_monolithic.hdf5 0 20
     --hdf5-sample-directory /data/final
 "
+
+export PMC_NUMBER_OF_JOBS=500
+export PMC_UNCERTAINTY_INPUT="${BASE_NAME}/sm_uncVLL/pmc_monolithic.hdf5"
+#export PMC_CLIENT_ARGV="--n-samples 500"
+export PMC_RESOURCE_MANAGER=loadleveler
+export LL_QUEUE=test
+export LL_FINAL_QUEUE=${LL_QUEUE}
+export PMC_POLLING_INTERVAL=2
 
 main $@
