7e4fa065b68e188104a3be5f2f56c96629b311e0
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2013-09-09 18:12:40 +0200

    [bayes2] Add loadleveler job scripts

---
+++ bayes2/loadleveler-monolithic.cmd
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+#@ group =  pr85tu
+#@ job_type = parallel
+#@ class = parallel
+###                    hh:mm:ss
+##@ wall_clock_limit = 40:55:50
+#@ node = 1
+#@ total_tasks = 1
+#@ node_usage = shared
+#@ resources = ConsumableCpus(12)
+#
+#@ job_name = eos-$(jobid)
+#@ initialdir = $(home)/workspace/eos-scripts/bayes2
+#@ output = /gpfs/work/pr85tu/ru72xaf2/log/$(jobid).out
+#@ error  = /gpfs/work/pr85tu/ru72xaf2/log/$(jobid).err
+#@ notification=always
+#@ notify_user=Frederik.Beaujean@lmu.de
+#@ queue
+. /etc/profile
+. /etc/profile.d/modules.sh
+
+export KMP_AFFINITY=granularity=core,compact,1
+
+# output file directory
+export BASE_NAME=$BASE_NAME/2013-08-28
+
+./sm-quim1.bash pmc
---
+++ bayes2/loadleveler-single.cmd
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+scenario=$1; shift
+constraints=$1; shift
+action=$1; shift
+
+for i in $(seq -f %01.0f 0 3); do
+    file=j${i}.job
+    # beware of shell escaping: loadlever variables
+    # must not be expanded by this shell
+    echo "#! /bin/bash
+#
+#@ group =  pr85tu
+#@ job_type = serial
+#@ class = serial
+##@ node = 1
+##@ total_tasks = 1
+##@ node_usage = shared
+##@ resources = ConsumableCpus(4)
+#
+###                    hh:mm:ss
+#@ wall_clock_limit = 10:15:50
+#@ job_name = eos-\$(jobid)
+#@ initialdir = \$(home)/workspace/eos-scripts/bayes2
+#@ output = /gpfs/work/pr85tu/ru72xaf2/log/\$(jobid).out
+#@ error  = /gpfs/work/pr85tu/ru72xaf2/log/\$(jobid).err
+#@ notification=always
+#@ notify_user=Frederik.Beaujean@lmu.de
+#@ queue
+. /etc/profile
+. /etc/profile.d/modules.sh
+
+export KMP_AFFINITY=granularity=core,compact,1
+
+# output file directory
+export BASE_NAME=$BASE_NAME/2013-09-04
+
+./${scenario}-${constraints}.bash ${action} $i" > $file
+
+    sync
+    llsubmit $file
+    rm $file
+done
