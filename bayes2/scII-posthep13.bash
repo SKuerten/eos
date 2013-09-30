7bb5ee700e9d1e39f08e788fc20e75ab966ff460
Danny van Dyk <danny.van.dyk@gmail.com>
2013-09-30 09:54:45 +0200

    [bayes2] Add scII-posthep13

---
+++ bayes2/scII-posthep13.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+#vim: set sts=4 et :
+
+source ${EOS_SCRIPT_PATH}/job.bash
+
+export MCMC_PRERUN_SAMPLES=40000
+export MCMC_PRERUN_UPDATE_SIZE=1000
+export MCMC_PRERUN_CHAINS=1
+export MCMC_PRERUN_PARALLEL=0
+
+export PMC_FINAL_CHUNKSIZE=1000000
+export PMC_CLUSTERS=35
+export PMC_CONVERGENCE="$PMC_CONVERGENCE --pmc-crop-highest-weights 200"
+
+export PMC_NUMBER_OF_JOBS=200
+export LL_QUEUE=test
+export LL_FINAL_QUEUE=serial
+export PMC_POLLING_INTERVAL=30
+
+
+main $@
