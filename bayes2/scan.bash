8c41419a39e7da537db14c8c28f44020c9d08987
Danny van Dyk <danny.van.dyk@gmail.com>
2013-09-30 09:48:25 +0200

    [bayes2] Repurpose scII for c9,c9'

--- bayes2/scan.bash
+++ bayes2/scan.bash
@@ -9,12 +9,8 @@ export SCAN_scI="
 "
 
 export SCAN_scII="
-        --scan      Re{c7}      -2.0     +2.0              --prior flat
-        --scan      Im{c7}      -2.0     +2.0              --prior flat
-        --scan      Re{c9}     -15.0    +15.0              --prior flat
-        --scan      Im{c9}     -15.0    +15.0              --prior flat
-        --scan      Re{c10}    -15.0    +15.0              --prior flat
-        --scan      Im{c10}    -15.0    +15.0              --prior flat
+        --scan      Re{c9}      -7.5     +7.5              --prior flat
+        --scan      Re{c9'}     -7.5     +7.5              --prior flat
 "
 
 export SCAN_scIII="
