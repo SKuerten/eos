91e03f264b35f23a09215d30c178f131a503d226
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2013-11-09 22:58:50 +0100

    [bayes2] Fix nuisance for posthep13hpqcd

--- bayes2/loadleveler-single.cmd
+++ bayes2/loadleveler-single.cmd
@@ -4,7 +4,7 @@ scenario=$1; shift
 constraints=$1; shift
 action=$1; shift
 
-for i in $(seq -f %01.0f 1 1); do
+for i in $(seq -f %01.0f 1 80); do
     file=j${i}.job
     # beware of shell escaping: loadlever variables
     # must not be expanded by this shell
@@ -33,7 +33,7 @@ for i in $(seq -f %01.0f 1 1); do
 export KMP_AFFINITY=granularity=core,compact,1
 
 # output file directory
-export BASE_NAME=$BASE_NAME/2013-09-12
+export BASE_NAME=$BASE_NAME/2013-11-09
 
 ./${scenario}-${constraints}.bash ${action} $i" > $file
 
--- bayes2/priors.bash
+++ bayes2/priors.bash
@@ -184,6 +184,7 @@ export NUISANCE_bsmumu="
 "
 
 export NUISANCE_posthep13=${NUISANCE_all}
+export NUISANCE_posthep13hpqcd=${NUISANCE_all}
 export NUISANCE_posthep13wide="
     ${NUISANCE_CKM}
     ${NUISANCE_QUARK_MASSES}
