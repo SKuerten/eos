77cf7fbc27ef41a5c5e596111c463f30f3bee0a3
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2014-04-23 14:44:30 +0200

    [bayes2] Run PMC for *hpqcdflat

--- bayes2/scII-posthep13hpqcdSLflat.bash
+++ bayes2/scII-posthep13hpqcdSLflat.bash
@@ -8,4 +8,14 @@ export MCMC_PRERUN_UPDATE_SIZE=500
 export MCMC_PRERUN_CHAINS=1
 export MCMC_PRERUN_PARALLEL=0
 
+export PMC_CLUSTERS=30
+export PMC_CHUNKSIZE=2000
+export PMC_CONVERGENCE="$PMC_CONVERGENCE --pmc-crop-highest-weights 200"
+
+export PMC_NUMBER_OF_JOBS=32
+export LL_QUEUE=shorttest
+export LL_FINAL_QUEUE=shorttest
+export PMC_POLLING_INTERVAL=30
+export PMC_CLIENT_ARGV=
+
 main $@
--- bayes2/sm-posthep13hpqcdSLflat.bash
+++ bayes2/sm-posthep13hpqcdSLflat.bash
@@ -8,4 +8,17 @@ export MCMC_PRERUN_UPDATE_SIZE=500
 export MCMC_PRERUN_CHAINS=1
 export MCMC_PRERUN_PARALLEL=0
 
+export PMC_CHUNKSIZE=2000
+export PMC_GROUP_BY_RVALUE=2
+export PMC_CLUSTERS=30
+export PMC_INITIALIZATION="$PMC_INITIALIZATION --pmc-r-value-no-nuisance 0"
+export PMC_CONVERGENCE="$PMC_CONVERGENCE --pmc-crop-highest-weights 50"
+export PMC_SKIP_INITIAL=0.2
+
+export PMC_NUMBER_OF_JOBS=150
+export PMC_RESOURCE_MANAGER=loadleveler
+export LL_QUEUE=test
+export LL_FINAL_QUEUE=serial
+export PMC_POLLING_INTERVAL=30
+
 main $@
