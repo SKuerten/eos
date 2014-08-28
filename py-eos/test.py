f27c393ad8f1004c167ac8fcb94cc8714dac7bda
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2014-08-28 13:50:46 +0200

    [py-eos] parse parameters and build analysis

--- py-eos/test.py
+++ py-eos/test.py
@@ -1,44 +1,18 @@
 #! /usr/bin/env python
 
+import eos
+import argparse
+import numpy as np
 from os import environ
 
-env_constraints = environ["CONSTRAINTS_posthep13"]
-
-# def get_option(line):
-#     expr = "--global-option "
-#     i = s.find(expr)
-#     if i != -1:
-#          s[i + len(expr) + 1:]
-
-# constraints = []
-# global_options = {}
-#
-# for s in env_constraints.splitlines():
-#     # only one option to eos-scan-mc per line, starts with "--"
-#     if not '--' in s:
-#         continue
-#     print s
-#     expr = "--global-option model"
-#     i = s.find(expr)
-#     if i != -1:
-#         print s[i + len(expr) + 1:]
-
-###
-# preprocessing:
-# 1. global-option -> local option (form factors)
-###
-
-# env_constraints = env_constraints.replace("--global-option form-factors ", "--local-option form-factors ")
-# env_constraints = env_constraints.replace("--global-option form-factors ", "--local-option form-factors ")
-
-for s in env_constraints.splitlines():
-    print s
-
-import argparse
-import eos
+# grab data defined in bash scripts
+scenario = "scIII"
+data = "posthep13"
+print environ
+cmd_line = environ["CONSTRAINTS_" + data] + environ["SCAN_" + scenario] + environ["NUISANCE_" + data]
 
 class ActionConstraint(argparse.Action):
-    """Store constraints together in order"""
+    """Store constraints and options together in order."""
     def __call__(self, parser, namespace, values, option_string=None):
         if not 'ordered_args' in namespace:
             setattr(namespace, 'ordered_args', [])
@@ -46,16 +20,32 @@ class ActionConstraint(argparse.Action):
         previous.append((self.dest, values))
         setattr(namespace, 'ordered_args', previous)
 
-parser = argparse.ArgumentParser(description="EOS scan mc emulator")
-parser.add_argument("--constraint", action=ActionConstraint)
-parser.add_argument("--global-option", nargs=2, action=ActionConstraint)
-parser.add_argument("--kinematics", action=ActionConstraint, nargs=2)
-parser.add_argument("--observable-prior", nargs=4, action=ActionConstraint)
-parser.add_argument("--nuisance")
-parser.add_argument("--prior")
-parser.add_argument("--scan", nargs=3)
+class ActionParameter(argparse.Action):
+    """Store parameter and prior definitions together in order."""
+    def __call__(self, parser, namespace, values, option_string=None):
+        if not 'ordered_params' in namespace:
+            setattr(namespace, 'ordered_params', [])
+        previous = namespace.ordered_params
+        previous.append((self.dest, values))
+        setattr(namespace, 'ordered_params', previous)
 
-args = parser.parse_args(env_constraints.split())
+parser = argparse.ArgumentParser(description="EOS scan mc emulator")
+
+parser.add_argument("--constraint", action=ActionConstraint)
+parser.add_argument("--global-option", action=ActionConstraint, nargs=2)
+parser.add_argument("--kinematics", action=ActionConstraint, nargs=2)
+parser.add_argument("--observable-prior", action=ActionConstraint, nargs=4)
+
+parser.add_argument("--nuisance", action=ActionParameter, nargs='*')
+parser.add_argument("--prior", action=ActionParameter, nargs='*')
+parser.add_argument("--scan", action=ActionParameter, nargs=3)
+
+args = parser.parse_args(cmd_line.split())
+
+if not hasattr(args, "ordered_args"):
+    raise Exception("No constraints found!")
+if not hasattr(args, "ordered_params"):
+    raise Exception("No parameters found!")
 
 options = {}
 constraints = []
@@ -73,16 +63,73 @@ for arg in args.ordered_args:
         kinematics[arg[1][0]] = float(arg[1][1])
     elif arg[0] == 'observable_prior':
         # ('observable_prior', ['B->K^*::V(s)/A_1(s)', '0.93', '1.33', '1.73'])
-        name, min, central, max = arg[1]
-        constraints.append(eos.ManualConstraint(name, float(min), float(central), float(max), 0, kinematics, **options))
+        name, mini, central, maxi = arg[1]
+        constraints.append(eos.ManualConstraint(name, float(mini), float(central), float(maxi), 0, kinematics, **options))
         # reset to empty kinematics for next observable
         kinematics = {}
     else:
-        raise UnknownArgument(arg)
+        raise Exception("Unknown constraint specification: " + str(arg))
 
-for c in constraints:
-    print c
+class PriorInfo(object):
+    """Store info for next prior while parsing."""
+    name = None
+    mini, maxi = -np.inf, np.inf
+    number = None
+    n_sigmas = None
+    nuisance = False
 
+priors = []
+pinfo = PriorInfo()
+# reimplement eos-scan-mc.cc
+for arg in args.ordered_params:
+    if arg[0] in ("scan", "nuisance"):
+        pinfo.nuisance = (arg[0] == "nuisance")
+        pinfo.name = arg[1][0]
+        if len(arg[1]) == 2:
+            pinfo.n_sigmas = float(arg[1][1])
+        else:
+            pinfo.mini, pinfo.maxi = float(arg[1][1]), float(arg[1][2])
+            if len(arg[1]) == 4:
+                pinfo.n_sigmas = float(arg[1][3])
+            elif len(arg[1]) > 4:
+                raise Exception("Invalid prior specification: " + str(arg))
+        if pinfo.n_sigmas is not None and (pinfo.n_sigmas < 0 or pinfo.n_sigmas > 10):
+            raise ValueError("Invalid n_sigma %d" % pinfo.n_sigmas)
+    elif arg[0] == "prior":
+        prior_type = arg[1][0]
+        prior = None
+        if prior_type in ("gaussian", "log-gamma"):
+            lower, central, upper = float(arg[1][1]), float(arg[1][2]), float(arg[1][3])
+            if pinfo.n_sigmas > 0:
+                pinfo.mini = max(pinfo.mini, central - pinfo.n_sigmas * (central - lower))
+                pinfo.maxi = min(pinfo.maxi, central + pinfo.n_sigmas * (upper - central))
+                call_args = (pinfo.name, pinfo.mini, pinfo.maxi, lower, central, upper)
+                call_kwargs = dict(nuisance=pinfo.nuisance)
+                if prior_type == "gaussian":
+                    prior = eos.LogPrior.Gauss(*call_args, **call_kwargs)
+                else:
+                    prior = eos.LogPrior.LogGamma(*call_args, **call_kwargs)
+        elif prior_type == "flat":
+            if pinfo.n_sigmas > 0:
+                raise Exception("Can't specify number of sigmas for flat prior")
+            prior = eos.LogPrior.Flat(pinfo.name, pinfo.mini, pinfo.maxi, nuisance=pinfo.nuisance)
+        else:
+            raise Exception("Unknown prior distribution: " + prior_type)
 
+        if not prior:
+            raise Exception("Prior could not be set for " + str(arg))
+        else:
+            priors.append(prior)
+            # reset name etc.
+            pinfo = PriorInfo()
+    else:
+        raise Exception("Unknown argument: " + str(arg))
 
+ana = eos.Analysis(constraints, priors)
 
+mode = np.array([+0.19279, -0.71658, -0.62039, +0.40307, -3.69668, -4.57915, +0.80256, +0.22532, +0.06386, +0.36407, +1.26332, +4.21015, +0.22937, +0.39652, -4.57418, +1.05489, +0.82686, +0.26285, -0.14794, +0.24709, -0.34462, -0.03520, +0.00607, -0.20922, +0.89997, +0.96355, +0.82546, +1.15800, +0.29384, -2.62888, +0.00153, +0.81901, +0.33862, 0.35404])
+
+print ana
+
+# target = 605.8885328
+print ana(mode)
