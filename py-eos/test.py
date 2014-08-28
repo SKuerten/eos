d3b88c90f0b85254e32c5d67c94864165a124f9f
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2014-08-28 09:42:28 +0200

    [py-eos] Constraint parsing completed

--- py-eos/test.py
+++ py-eos/test.py
@@ -34,12 +34,11 @@ env_constraints = environ["CONSTRAINTS_posthep13"]
 for s in env_constraints.splitlines():
     print s
 
-
-
 import argparse
 import eos
 
 class ActionConstraint(argparse.Action):
+    """Store constraints together in order"""
     def __call__(self, parser, namespace, values, option_string=None):
         if not 'ordered_args' in namespace:
             setattr(namespace, 'ordered_args', [])
@@ -47,37 +46,43 @@ class ActionConstraint(argparse.Action):
         previous.append((self.dest, values))
         setattr(namespace, 'ordered_args', previous)
 
-class ActionKinematics(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        if not 'ordered_kinematics' in namespace:
-            setattr(namespace, 'ordered_kinematics', [])
-        previous = namespace.ordered_kinematics
-        previous.append((self.dest, values))
-        setattr(namespace, 'ordered_kinematics', previous)
-
 parser = argparse.ArgumentParser(description="EOS scan mc emulator")
 parser.add_argument("--constraint", action=ActionConstraint)
 parser.add_argument("--global-option", nargs=2, action=ActionConstraint)
-parser.add_argument("--kinematics", action=ActionKinematics, nargs=2)
+parser.add_argument("--kinematics", action=ActionConstraint, nargs=2)
+parser.add_argument("--observable-prior", nargs=4, action=ActionConstraint)
 parser.add_argument("--nuisance")
-parser.add_argument("--observable-prior", nargs=4, action=ActionKinematics)
 parser.add_argument("--prior")
 parser.add_argument("--scan", nargs=3)
 
 args = parser.parse_args(env_constraints.split())
 
-print args.__dict__
 options = {}
 constraints = []
-for oa in args.ordered_args:
-    if oa[0] == 'global_option':
-        options[oa[1][0]] = oa[1][1]
-    if oa[0] == "constraint":
-        constraints.append(eos.Constraint(oa[1], **options))
+kinematics = {}
 
-print args.ordered_kinematics
-# for ok in args.ordered_kinematics:
-#     print
+for arg in args.ordered_args:
+    if arg[0] == 'global_option':
+        # ('global_option', ['model', 'WilsonScan'])
+        options[arg[1][0]] = arg[1][1]
+    elif arg[0] == "constraint":
+        # ('constraint', "B^0->K^*0mu^+mu^-::P'_5[14.18,16.00]@LHCb-2013")
+        constraints.append(eos.Constraint(arg[1], **options))
+    elif arg[0] == "kinematics":
+        # ('kinematics', ['s', '0.0'])
+        kinematics[arg[1][0]] = float(arg[1][1])
+    elif arg[0] == 'observable_prior':
+        # ('observable_prior', ['B->K^*::V(s)/A_1(s)', '0.93', '1.33', '1.73'])
+        name, min, central, max = arg[1]
+        constraints.append(eos.ManualConstraint(name, float(min), float(central), float(max), 0, kinematics, **options))
+        # reset to empty kinematics for next observable
+        kinematics = {}
+    else:
+        raise UnknownArgument(arg)
+
+for c in constraints:
+    print c
+
 
 
 
