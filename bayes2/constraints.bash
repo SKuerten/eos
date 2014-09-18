47d25f1153b8b0985c2334ac74af15558e247c4a
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2014-09-18 19:10:57 +0200

    [bayes2] Add B->K mu mu A_FB from CDF2012 to ONSTRAINTS_B_TO_PMUMU_*_RECOIL_A_FB

--- bayes2/constraints.bash
+++ bayes2/constraints.bash
@@ -91,10 +91,13 @@ export CONSTRAINTS_B_TO_PMUMU_LOW_RECOIL_BR="
 "
 
 export CONSTRAINTS_B_TO_PMUMU_LARGE_RECOIL_A_FB="
+    --constraint B^+->K^+mu^+mu^-::BR[1.00,6.00]@CDF-2012
     --constraint B^+->K^+mu^+mu^-::A_FB[1.10,6.00]@LHCb-2014
 "
 
 export CONSTRAINTS_B_TO_PMUMU_LOW_RECOIL_A_FB="
+    --constraint B^+->K^+mu^+mu^-::BR[14.18,16.00]@CDF-2012
+    --constraint B^+->K^+mu^+mu^-::BR[16.00,22.86]@CDF-2012
     --constraint B^+->K^+mu^+mu^-::A_FB[15.00,22.00]@LHCb-2014
 "
 
