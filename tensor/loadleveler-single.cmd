d3a3719fadda5d3cea8092e76e486cacbbe31f39
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2014-11-28 19:53:45 +0100

    [tensor] run first mcmc jobs on c2pap

---
+++ tensor/loadleveler-single.cmd
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# examples:
+# BASE_NAME=$BASE_NAME/2014-02-07 ./loadleveler-single.cmd scI posthep13hpqcdnoFLBabarAtlas gof 0 1
+# BASE_NAME=$BASE_NAME/2014-02-07 ./loadleveler-single.cmd scIII posthep13hpqcdSLflat pre 0 10
+
+scenario=$1; shift
+constraints=$1; shift
+action=$1; shift
+low=$1; shift
+high=$1; shift
+
+for i in $(seq -f %01.0f $low $high); do
+    file=j${i}.job
+    # beware of shell escaping: loadlever variables
+    # must not be expanded by this shell
+    echo "#! /bin/bash
+#
+#@ group = pr85tu
+#@ job_type = serial
+#@ class = fat
+#@ node_usage = shared
+#@ resources = ConsumableCpus(1)
+#
+###                    hh:mm:ss
+#@ wall_clock_limit = 47:59:50
+#@ job_name = eos-\$(jobid)
+#@ initialdir = \$(home)/workspace/eos-scripts/tensor
+#@ output = /gpfs/work/pr85tu/ru72xaf2/log/\$(jobid).out
+#@ error  = /gpfs/work/pr85tu/ru72xaf2/log/\$(jobid).err
+#@ notification=error
+#@ notify_user=Frederik.Beaujean@lmu.de
+#@ queue
+
+# output file directory
+export BASE_NAME=$BASE_NAME
+
+./${scenario}-${constraints}.bash ${action} $i" > $file
+
+    sync
+    llsubmit $file
+    rm $file
+done
--- tensor/scTT5-K_KstarBR_Bsmumu.bash
+++ tensor/scTT5-K_KstarBR_Bsmumu.bash
@@ -2,4 +2,10 @@
 
 source ${EOS_SCRIPT_PATH}/job.bash
 
+export EOS_MCMC_SCALE_REDUCTION=3
+export EOS_MCMC_BURN_IN=500
+export EOS_MCMC_UPDATE_SIZE=500
+export EOS_MCMC_SAMPLES=40000
+export EOS_MCMC_PROPOSAL='cauchy'
+
 main $@
