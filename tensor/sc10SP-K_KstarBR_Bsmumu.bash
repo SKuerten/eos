86464f91a8db612dfa0c6f75adca16a8852d8275
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2015-03-09 14:40:26 +0100

    [tensor] Submit (pseudo)scalar jobs on c2pap

--- tensor/sc10SP-K_KstarBR_Bsmumu.bash
+++ tensor/sc10SP-K_KstarBR_Bsmumu.bash
@@ -6,15 +6,15 @@ export EOS_ANALYSIS_INFO=0
 
 export EOS_IS_SAMPLES=10000
 
-export EOS_MCMC_SCALE_REDUCTION=5
-export EOS_MCMC_BURN_IN=200
-export EOS_MCMC_UPDATE_SIZE=500
-export EOS_MCMC_SAMPLES=50000
+export EOS_MCMC_BURN_IN=500
 export EOS_MCMC_PROPOSAL='gauss'
+export EOS_MCMC_SAMPLES=50000
+export EOS_MCMC_SCALE_REDUCTION=5
+export EOS_MCMC_UPDATE_SIZE=500
 
 export EOS_VB_COMPONENTS_PER_GROUP=3
 export EOS_VB_EXTRA_OPTIONS='--indices 0 2 --rel-tol 0.000001'
-export EOS_VB_MCMC_INPUT='/data/eos/2014-tensor/2015-01-15/scSP_Bsmumu/mcmc_pre_merged.hdf5'
+#export EOS_VB_MCMC_INPUT='/data/eos/2014-tensor/2015-01-15/scSP_Bsmumu/mcmc_pre_merged.hdf5'
 #export EOS_VB_IS_INPUT='/data/eos/2014-tensor/2014-12-12/scTT5_FH/is.hdf5'
 export EOS_VB_PRUNE=100
 export EOS_VB_R_VALUE=2
