1037d6a733e6fe092e9852ead60a0914feb92735
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2015-05-11 12:29:18 +0200

    [lattice-data/parse] plot results for validation

--- tensor/lattice-synthetic-data/parse.py
+++ tensor/lattice-synthetic-data/parse.py
@@ -2,7 +2,9 @@ from __future__ import print_function
 import numpy as np
 np.set_printoptions(precision=10, linewidth=100, threshold=50**2)
 
-def form_factors(file_name, systematic=0.05):
+from matplotlib import pyplot as plt
+
+def parse_form_factors(file_name, systematic=0.05):
     print('opening', file_name)
     with open(file_name) as f:
         lines = f.readlines()
@@ -34,13 +36,11 @@ def form_factors(file_name, systematic=0.05):
         # add systematic uncertainty
         covariance += np.outer(std_dev, std_dev)
 
-        # correlation matrix
-        tmp = np.sqrt(covariance.diagonal())
-        tmp = np.outer(tmp, tmp)
-        corr = covariance / tmp
-
         # standard deviation
         std_dev = np.sqrt(covariance.diagonal())
+
+        # correlation matrix
+        corr = covariance / np.outer(std_dev, std_dev)
 
         print('mean')
         print(repr(mean))
@@ -55,7 +55,54 @@ def form_factors(file_name, systematic=0.05):
             np.linalg.cholesky(covariance)
             print('Cholesky works')
         except np.LinalgError:
-            print('Cholesky does NOT work')
+            raise
 
-form_factors('ff_phys_mass_av_sl.d')
-form_factors('ff_phys_mass_t_sl.d')
+        return mean, std_dev, corr, indices
+
+def parse_qsquared(file_name):
+    '''Parse (physical) q^2 values for each configuration'''
+    res = {}
+    with open(file_name) as f:
+        for l in f:
+            k, v = l.split()
+            pos1 = k.find('_l')
+            res[k[pos1+1:]] = v
+    return res
+
+def match_qsquared(indices, qsquared):
+    '''Match q^2 for each FF value given configuration name.
+    Return array with order of ``indices`` that contains the right q^2 value.'''
+    res = np.zeros(len(indices))
+    for k,v in indices.iteritems():
+        pos1 = k.find('_sl_l')
+        res[v] = qsquared[k[pos1 + 4:]]
+    return res
+
+def plot_ff(tensor=False):
+    tag = 't' if tensor else 'av'
+
+    # parse data
+    mean, std_dev, corr, indices = parse_form_factors('ff_phys_mass_' + tag + '_sl.d')
+    qsquared_dict = parse_qsquared('qsqr_phys_mass_' + tag + '_sl.d')
+    qsquared = match_qsquared(indices, qsquared_dict)
+
+    labels = ['$T_1$', '$T_2$', '$T_{23}$'] if tensor else ['$V$', '$A_1$', '$A_{12}$', '$A_0$']
+    delta_mF = [0.135, 0.55, 0.55] if tensor else [0.135, 0.550, 0.550, 0.087]
+    npoints = len(qsquared) / len(labels)
+
+    plt.clf()
+    for i, l in enumerate(labels):
+        slice = np.s_[i*npoints:(i+1)*npoints]
+        blaschke =  1 / (1 - qsquared[slice] / (5.27958 + delta_mF[i])**2)
+        plt.errorbar(qsquared[slice], blaschke * mean[slice], yerr=blaschke * std_dev[slice], fmt='o', label=l)
+
+    plt.legend()
+    plt.xlim(11, 19.21)
+    plt.ylim(0, 2)
+    plt.xlabel(r'$q^2$')
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(tag + '.pdf')
+
+plot_ff(tensor=True)
+plot_ff(tensor=False)
