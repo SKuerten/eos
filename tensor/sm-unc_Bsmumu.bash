efca34676d0eff1afea0c280e471d9ca76fc2444
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2015-06-10 11:37:45 +0200

    [tensor] Add all observables for prior predictive checks

--- tensor/sm-unc_Bsmumu.bash
+++ tensor/sm-unc_Bsmumu.bash
@@ -13,9 +13,4 @@ export EOS_MCMC_SAMPLES=100000
 export EOS_MCMC_SCALE_REDUCTION=5
 export EOS_MCMC_UPDATE_SIZE=500
 
-# export EOS_UNC_EXTRA_ARGS=''
-export EOS_UNC_OBSERVABLE='B_q->ll::BR@Untagged,q=s,l=mu'
-export EOS_UNC_INPUT="$BASE_NAME/sm_unc_Bsmumu/mcmc_pre_merged.hdf5"
-export EOS_UNC_FIX='Re{c10} -4.5 -4.0 11'
-
 main $@
--- tensor/unc_scenario.bash
+++ tensor/unc_scenario.bash
@@ -3,3 +3,6 @@ export UNC_PAR_ct=
 
 export UNC_FIX_ct_c9_1dot1=$UNC_FIX_ct
 export UNC_PAR_ct_c9_1dot1="Re{c9} -1.1"
+
+export UNC_FIX_fix_sm="Re{cT} 0.0 0.1 1"
+export UNC_PAR_fix_sm=
