ea8d149aec9f4140a0aa26cf53e80a3e029ea5f6
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2015-06-15 15:58:18 +0200

    [tensor] start sm-K_KstarBR_Bsmumu.bash with fixed initial value

--- tensor/sm-K_KstarBR_Bsmumu.bash
+++ tensor/sm-K_KstarBR_Bsmumu.bash
@@ -7,9 +7,9 @@ export EOS_ANALYSIS_INFO=0
 
 export EOS_MCMC_BURN_IN=
 export EOS_MCMC_COVARIANCE="Kstar-FF-cov.txt"
-export EOS_MCMC_INITIAL_VALUES="uniform"
+export EOS_MCMC_INITIAL_VALUES="fixed"
 export EOS_MCMC_PROPOSAL='gauss'
-export EOS_MCMC_SAMPLES=500000
+export EOS_MCMC_SAMPLES=150000
 export EOS_MCMC_UPDATE_SIZE=500
 
 main $@
