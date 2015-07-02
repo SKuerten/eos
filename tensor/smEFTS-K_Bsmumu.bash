1b4bf2ed6bbfa8aea94599f48fa1b204463910d6
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2015-07-02 11:00:06 +0200

    [tensor] add `K_Bsmumu`, remove `test*`, add *noSL job script

--- tensor/smEFTS-K_Bsmumu.bash
+++ tensor/smEFTS-K_Bsmumu.bash
@@ -2,10 +2,9 @@
 
 source ${EOS_SCRIPT_PATH}/job.bash
 
-export EOS_SEED=8793218
+export EOS_SEED=9217324
 export EOS_ANALYSIS_INFO=1
 
-export EOS_MCMC_COVARIANCE="Kstar-FF-cov.txt"
 export EOS_MCMC_INITIAL_VALUES="fixed"
 export EOS_MCMC_PROPOSAL='gauss'
 export EOS_MCMC_SAMPLES=150000
