9d70272a9130c5c76160be523cd903397719e780
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2015-07-02 11:48:37 +0200

    [tensor] Add `post_pred` for posterior predictive uncertainty
    add observables
    * Kstar_J_6c
    * Bsmumu_A_DeltaGamma

--- tensor/unc_observable.bash
+++ tensor/unc_observable.bash
@@ -70,6 +70,16 @@ export UNC_KIN_Kstar_J_1s_minus_3J_2s1to6=${UNC_KIN_Kstar_BR1to6}
 export UNC_OBS_Kstar_J_1s_minus_3J_2s15to19="B->K^*ll::J_1s-3J_2savg@LowRecoil${GLOBAL_OPTIONS}${KSTAR_OPTIONS}"
 export UNC_KIN_Kstar_J_1s_minus_3J_2s15to19=${UNC_KIN_Kstar_J_1c_plus_J_2c15to19}
 
+
+export UNC_OBS_Kstar_J_6c1to6="B->K^*ll::J_6cavg@LargeRecoil${GLOBAL_OPTIONS}${KSTAR_OPTIONS}"
+export UNC_KIN_Kstar_J_6c1to6=${UNC_KIN_Kstar_BR1to6}
+
+export UNC_OBS_Kstar_J_6c15to19="B->K^*ll::J_6cavg@LowRecoil${GLOBAL_OPTIONS}${KSTAR_OPTIONS}"
+export UNC_KIN_Kstar_J_6c15to19=${UNC_KIN_Kstar_J_1c_plus_J_2c15to19}
+
+
+export UNC_OBS_Bsmumu_A_DeltaGamma="B_q->ll::A_DeltaGamma${GLOBAL_OPTIONS},q=s,l=mu"
+
 # compare for debugging
 export UNC_OBS_Kstar_ACP1dot1to6="B->K^*ll::A_CP@LargeRecoil${GLOBAL_OPTIONS}${KSTAR_OPTIONS}"
 export UNC_KIN_Kstar_ACP1dot1to6="s_min 1.1 s_max 6"
@@ -87,6 +97,3 @@ export UNC_OBS_Kstar_AFB16to19="B->K^*ll::A_FBavg@LowRecoil${GLOBAL_OPTIONS}${KS
 export UNC_KIN_Kstar_AFB16to19="s_min 16 s_max 19"
 
 export UNC_OBS_Bsmumu_BR="B_q->ll::BR@Untagged${GLOBAL_OPTIONS},q=s,l=mu"
-
-export Kobs="BR14dot18to16 BR16to22dot86 AFB1dot1to6 AFB14dot18to16 AFB16to22dot86 AFB15to22 ACP1dot1to6 ACP15to22"
-export Kstarobs="ACP1dot1to6 ACP15to19 AFB1dot1to6 AFB14dot18to16 AFB16to19"
